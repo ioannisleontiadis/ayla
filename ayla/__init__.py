c4b32b15038cca58750cc38f14bd22afb442539b
ioannisleo <trickle-takes06@icloud.com>
2024-09-27 01:47:04 +0300

    scary

--- ayla/__init__.py
+++ ayla/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from . import db
+from . import db, home
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    app.teardown_appcontext(close_db)
+    app.teardown_appcontext(db.close_db)
 
     app.register_blueprint(home.bp)
 
